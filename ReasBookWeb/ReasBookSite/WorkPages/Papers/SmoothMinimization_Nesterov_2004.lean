30c60076a28ff8d828830b4b805ba2dc5e0d4969
wl-ma <mawanli96@gmail.com>
2026-08-08 21:06:40 +0800

    v4.30.0: fix gen_sections.py Papers module prefix and maxRecDepth
    - Fix to_module() and route_from_source() to also strip 'Papers.' prefix
      (module names like 'Papers.OnSomeLocalRings_Maassaran_2025.Paper' are
      invalid since lean_lib Papers was removed from lakefile)
    - Add set_option maxRecDepth 200000 to generated Sections.lean to handle
      large array literal with 3895 section entries

--- ReasBookWeb/ReasBookSite/WorkPages/Papers/SmoothMinimization_Nesterov_2004.lean
+++ ReasBookWeb/ReasBookSite/WorkPages/Papers/SmoothMinimization_Nesterov_2004.lean
@@ -3,13 +3,13 @@ open Verso Genre Blog
 
 #doc (Page) "Smooth Minimization (Nesterov, 2004)" =>
 
-- [Documentation](/ReasBook/docs/Papers/SmoothMinimization_Nesterov_2004/Paper.html)
+- [Documentation](/ReasBook/docs/SmoothMinimization_Nesterov_2004/Paper.html)
 - [Lean source path](https://github.com/optpku/ReasBook/tree/main/ReasBook/Papers/SmoothMinimization_Nesterov_2004/Sections/)
 
 Section index:
 
-- [Section 1: Introduction](/ReasBook/papers/smoothminimization_nesterov_2004/sections/section01/)
-- [Section 2: Smooth Approximations of Non-differentiable Functions](/ReasBook/papers/smoothminimization_nesterov_2004/sections/section02/)
-- [Section 3: Fast Gradient Methods](/ReasBook/papers/smoothminimization_nesterov_2004/sections/section03/)
-- [Section 4: Applications](/ReasBook/papers/smoothminimization_nesterov_2004/sections/section04/)
-- [Section 5: Implementation Issues and Modifications](/ReasBook/papers/smoothminimization_nesterov_2004/sections/section05/)
+- [Section 1: Introduction](/ReasBook/smoothminimization_nesterov_2004/sections/section01/)
+- [Section 2: Smooth Approximations of Non-differentiable Functions](/ReasBook/smoothminimization_nesterov_2004/sections/section02/)
+- [Section 3: Fast Gradient Methods](/ReasBook/smoothminimization_nesterov_2004/sections/section03/)
+- [Section 4: Applications](/ReasBook/smoothminimization_nesterov_2004/sections/section04/)
+- [Section 5: Implementation Issues and Modifications](/ReasBook/smoothminimization_nesterov_2004/sections/section05/)
