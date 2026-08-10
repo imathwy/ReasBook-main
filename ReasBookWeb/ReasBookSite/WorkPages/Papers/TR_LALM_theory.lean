eabad7114a802925218da97abaa7261bd085ebaa
imathwy <3124237569@qq.com>
2026-08-10 19:37:10 +0800

    [Paper][v4.32.2] Add curated TR-LALM theorem map

--- ReasBookWeb/ReasBookSite/WorkPages/Papers/TR_LALM_theory.lean
+++ ReasBookWeb/ReasBookSite/WorkPages/Papers/TR_LALM_theory.lean
@@ -6,7 +6,7 @@ open Verso Genre Blog
 - [Documentation](/ReasBook/docs/TR_LALM_theory/Paper.html)
 - [Lean source path](https://github.com/optpku/ReasBook/tree/v4.32.2/ReasBook/Papers/TR_LALM_theory/)
 - [Formalization README](https://github.com/optpku/ReasBook/blob/v4.32.2/ReasBook/Papers/TR_LALM_theory/README.md)
-- [Interactive theorem dependency map](https://imathwy.github.io/ReasBook-TR_LALM/theorem-map/)
+- [Interactive theorem dependency map](https://optpku.github.io/ReasBook/theorem-maps/papers/tr_lalm_theory/)
 
 - 2,853 declarations across 141 implementation files, with no `sorry` or
   `admit` placeholders.
